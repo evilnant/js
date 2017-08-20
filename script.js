208809fe3242f7483458733abb3b5378ba546b61
evilnant <30756585+evilnant@users.noreply.github.com>
2017-08-20 22:17:08 +0300

    123

---
+++ script.js
@@ -0,0 +1,26 @@
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var z = 0;
+
+for (var x = 0; x < 10; x++) {
+    var a = (getRandomInRange(1, 9));
+    var b = (getRandomInRange(1, 9));
+    var res = a * b;
+
+    var vod = (prompt(a + " * " + b + " =? "));
+
+
+
+    if (vod == res) {
+        alert("Правильно");
+        z = z + 1
+
+    } else {
+        alert("Неправильно ");
+
+    }
+
+}
+alert("Вы дали " + z + " правильных ответов из 10")
