f05d70bb3b38258aae44e9c74d5fbd2102cf5688
evilnant <lazyrgin@gmail.com>
2017-08-21 14:09:24 +0300

    dopilil

--- script/script.js
+++ script/script.js
@@ -2,25 +2,36 @@ function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var z = 0;
-
-for (var x = 0; x < 4; x++) {
-    var a = (getRandomInRange(1, 20));
-    var b = (getRandomInRange(1, 99));
-    var res = a * b;
-
-    var vod = (prompt(a + " * " + b + " =? "));
-
-
-
-    if (vod == res) {
-        alert("Правильно");
-        z = z + 1
-
-    } else {
-        alert("Неправильно ");
-
+var want = confirm("Хочешь пройти испытание?");
+if (want == true) {
+    var z = 0;
+    var count = prompt("Какое количество испытаний?")
+    
+    for (var x = 0; x < count; x++) {
+        var a = (getRandomInRange(1, 9));
+        var b = (getRandomInRange(1, 9));
+        var res = a * b;
+    
+        var vod = (prompt(a + " * " + b + " =? "));
+    
+    
+            
+        while(vod != res) {
+            alert("Неправильно, попробуй ещё =)");
+            vod = (prompt(a + " * " + b + " =? "));
+            z = z + 1       
+        }
+        
+        if (vod == res) {
+            alert("Правильно, молодец!");
+           
+        }
+        
+    
+    
     }
-
+    alert("Вы ошиблись " + z + " раз")
+    
 }
-alert("Вы дали " + z + " правильных ответов из 4")
+
+
