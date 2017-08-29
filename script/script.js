e5a32005a026ca6f7678c96ca7b691b4b191055b
evilnant <lazyrgin@gmail.com>
2017-08-29 23:40:07 +0300

    123123123

--- script/script.js
+++ script/script.js
@@ -2,29 +2,41 @@ function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function mathOp(a, b, op) {
+    if (op == "-") {
+        var res = a - b;
+    } else if (op == "+") {
+        res = Number(a) + Number(b);
+    } else if (op == "*") {
+        res = a * b;
+    }
+
+    return res;
+}
+
 var want = confirm("Хочешь пройти испытание?");
 if (want == true) {
     var z = 0;
 
-    var slo = prompt("Выберите сложность от 1 до 3?")
+    var slo = prompt("Выберите сложность от 1 до 3?");
 
     while ((slo > 3) || (slo < 1) || (isNaN(slo))) {
-        var slo = prompt("           Неверное значение \n Выберите сложность от 1 до 3? ")
-    };
+        var slo = prompt("           Неверное значение \n Выберите сложность от 1 до 3? ");
+    }
 
-    var count = prompt("Какое количество испытаний?")
+    var count = prompt("Какое количество испытаний?");
     while (isNaN(count)) {
-        count = prompt("           Неверное значение \nКакое количество испытаний?")
-    };
+        count = prompt("           Неверное значение \nКакое количество испытаний?");
+    }
 
     if(count == 3 || 4 || 2 || 22 || 23 || 24 || 32 || 33 || 34 || 42 || 43 || 44 ||
         52 || 53 || 54 || 62 || 63 || 64) {
-        slovo = " примера."
+        slovo = " примера.";
     }else if (count == 1 || 21 || 31 || 41 || 51 || 61) {
-        slovo = " пример."
+        slovo = " пример.";
     } else {
-        slovo = " примеров."
-    };
+        slovo = " примеров.";
+    }
 
     if (slo == 1) {
         var zlo = 9;
@@ -41,7 +53,7 @@ if (want == true) {
         var OK = 4;
     }
 
-    alert("Вы выбрали " + slo + "-ую сложность и " + count + slovo +" У вас "+(OK+1)+ " попытки на каждый пример.")
+    alert("Вы выбрали " + slo + "-ую сложность и " + count + slovo +" У вас "+(OK+1)+ " попытки на каждый пример.");
 
 
 
@@ -65,19 +77,9 @@ if (want == true) {
         }
 
 
-        function mathOp(a, b, op) {
-            if (op == "-") {
-                var res = a - b;
-            } else if (op == "+") {
-                res = Number(a) + Number(b);
-            } else if (op == "*") {
-                res = a * b;
-            }
 
-            return res;
-        };
 
-        res = mathOp(a, b, op)
+        res = mathOp(a, b, op);
 
         var vod = (prompt("                 " + (x + 1) + "/" + count + " испытание. \n \n        " + a + " " + op + " " + b + " =? " + "             Попыток : " + (OK+1)));
 
@@ -91,7 +93,7 @@ if (want == true) {
             for (OK; OK > 0; OK--) {
                 alert("Неправильно, попробуй ещё раз!");
                 vod = (prompt( "           " + (x + 1) + "/" + count + " испытание. \n \n"+ a + " " + op + " " + b + " =? " + "             Попыток : " + OK));
-                z = z + 1
+                z = z + 1;
                 if (vod == res) {
                     alert("Правильно, молодец!");
                     break;
@@ -99,16 +101,16 @@ if (want == true) {
                 
             }
             break;
-        };
+        }
        
 
-    };
+    }
     if (z == 0) {
-        alert("Поздравляю, вы завершили испытания без ошибок!")
+        alert("Поздравляю, вы завершили испытания без ошибок!");
     }else if (OK == 0) {
-        alert("Правильнйы ответ : " + a + " " + op + " " + b + " = " + res + "\n ВЫ НЕ ПРОШЛИ ИСПЫТАНИЕ =(")
+        alert("Правильнйы ответ : " + a + " " + op + " " + b + " = " + res + "\n ВЫ НЕ ПРОШЛИ ИСПЫТАНИЕ =(");
     } else{
-        alert("Поздравляю, вы завершили испытание ошибившись " + z + " раз(a), в следуюший раз будет лучше!")
+        alert("Поздравляю, вы завершили испытание ошибившись " + z + " раз(a), в следуюший раз будет лучше!");
     }
     
 
